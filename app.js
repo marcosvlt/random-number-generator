ea67a6b529546838b7fea187c89e5f623ff3185c
marcosvlt <msr.airborne@gmail.com>
2024-10-09 14:53:31 -0300

    feature raffle not repeated numbers and result change

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 function raffle() {
     let quantity = parseInt(document.getElementById('quantity').value);
     let from = parseInt(document.getElementById('from').value);
-    let until = parseInt(document.getElementById('quantity').value);
+    let until = parseInt(document.getElementById('until').value);
 
-    console.log(`quantity = ${quantity}` );
-    console.log(`from = ${from}` );
-    console.log(`until = ${until}` );
+    // console.log(`quantity = ${quantity}` );
+    // console.log(`from = ${from}` );
+    // console.log(`until = ${until}` );
 
     let number; 
-
     let drawn = [];
 
-    for (let index = from; index < quantity; index++) {
+    for (let index = 0; index < quantity; index++ ) {
         number = getRndInteger(from, until);
+        
+        while(drawn.includes(number)){
+            number = getRndInteger(from,until);
+        }
+
         drawn.push(number);
         
     }
 
-    console.log(`drawn = ${drawn}` );
-
-
-
+    // console.log(`drawn = ${drawn}` );
+    resultChange(drawn);
  
 }
 
@@ -28,5 +30,11 @@ function getRndInteger(min, max){
 
     return Math.floor(Math.random() * (max - min))  + min;
 
+}
 
+function resultChange(draw) {
+
+    let result = document.getElementById('result');
+    result.innerHTML = `<label class="text_paragraph">Drawn numbers: ${draw}</label>`;
+    
 }
